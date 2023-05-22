e4ae020977a598d8a3306eff8247e5e61a2d6515
yunawinaya <111243019+yunawinaya@users.noreply.github.com>
2023-05-22 05:24:40 +0000

    Added portofolio section & images

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+var sidemeu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemeu.style.right = "0";
+}
+function closemenu(){
+    sidemeu.style.right = "-200px";
+}
