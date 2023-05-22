24510c186bd014fca98543fe38fabb8fe8c61bb0
yunawinaya <111243019+yunawinaya@users.noreply.github.com>
2023-05-22 05:58:25 +0000

    Added contact section, CV file, and script to receive the message from contact form

--- script.js
+++ script.js
@@ -20,3 +20,20 @@ function openmenu(){
 function closemenu(){
     sidemeu.style.right = "-200px";
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxQylcCwA2I1oiSvlO8X0XN_tIqTf6Ec5uVFT60xucTLdOJl8ej-JNWE_hiRvkM6iDQ/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+  
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
